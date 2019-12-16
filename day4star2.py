a8c9330cca5d5bd0c7443408a6653e215fc6ed7d
rileyln <nairnr@oregonstate.edu>
2019-12-15 20:37:37 -0500

    Implemented day4star2.py

--- day4star2.py
+++ day4star2.py
@@ -13,21 +13,36 @@ def digits_not_decreasing(number):
                 return False
 
 
-def two_adjacent_same_digits(number):
+def only_two_adjacent_same_digits(number):
     number_string = str(number)
     previous_digit = number_string[0]
+    digit_groups = {
+        "0" : 0,
+        "1" : 0,
+        "2" : 0,
+        "3" : 0,
+        "4" : 0,
+        "5" : 0,
+        "6" : 0,
+        "7" : 0,
+        "8" : 0,
+        "9" : 0
+    }
     for digit in range(1, len(number_string)):
         if number_string[digit] == previous_digit:
-            return True
+            digit_groups[number_string[digit]] += 1
         else:
             previous_digit = number_string[digit]
+    for digit in digit_groups:
+        if digit_groups[digit] == 1:
+            return True
     return False
 
 
 potential_passwords = set()
 
 for password in range(254032, 789860):
-    if digits_not_decreasing(password) and two_adjacent_same_digits(password):
+    if digits_not_decreasing(password) and only_two_adjacent_same_digits(password):
         potential_passwords.add(password)
 
 print(len(potential_passwords))
